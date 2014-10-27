dd66238592cdd65ec9f92dcff55da402b8fa5e55
Chris Ian Fiel <ccfiel@gmail.com>
2014-10-27 09:17:23 +0800

    change to production

--- sample_number.php
+++ sample_number.php
@@ -2,7 +2,7 @@
 $data = array("user_email" => "ccfiel@surebooked.com", "user_token" => "BUuqDCDPyUoGSvtCoFzX", "mobile_number" => "+639177048787", "message" => "test", "pay" => true);                                                                    
 $data_string = json_encode($data);                                                                                   
  
-$ch = curl_init('http://staging.smsblastr.com/api/v1/send_number');                                                                      
+$ch = curl_init('http://smsblastr.com/api/v1/send_number');                                                                      
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
